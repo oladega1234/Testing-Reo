dbd17effb72e98cafc6c457f6bb3c995c4bf4f11
oladega1234 <oladega@sheridancollege.ca>
2023-04-16 14:15:30 -0400

    Change Name

--- src/hockeyregistrar/HockeyRegistrarValidator.java
+++ src/hockeyregistrar/HockeyRegistrarValidator.java
@@ -18,7 +18,7 @@ public class HockeyRegistrarValidator
     /**
      * A method that validates that players are eligible for '10 and under' meaning they must be born in 2009 or later .
      * @param playerBirthYear, the player's year of birth
-     * @return true if the player was born in 2009 or later, false otherwise. 
+     * @return true if the player wasdddfs born in 2009 or later, false otherwise. 
      */
     public boolean validateBirthYear(int playerBirthYear)
     {
